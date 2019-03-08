419b50889958b7728ff354030840f22b68cf0642
Łukasz Górski <lukeg5@gmail.com>
2019-03-08 21:30:43 +0100

    Downloading MNIST images.

--- python/download_mnist.py
+++ python/download_mnist.py
@@ -1,3 +1,15 @@
 if __name__ == "__main__":
-    from sklearn.datasets import fetch_mldata
-    mnist = fetch_mldata('MNIST original', data_home='../datasets/mnist')
+    import tensorflow as tf
+    import typing
+    def save_pixels_and_labels (X, y, save: typing.TextIO):
+        for pixels, label in zip (X, y):
+            pixels_list = [str(num) for num in (pixels.reshape(-1, 784)/255).tolist()[0]]
+            save.write("{} {}\n".format(label, " ".join(pixels_list)))
+
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    with open("../mnist.train.txt", "w") as filehandle:
+        save_pixels_and_labels(X_train, y_train, filehandle)
+    with open("../mnist.test.txt", "w") as filehandle:
+        save_pixels_and_labels(X_test, y_test, filehandle)
+
